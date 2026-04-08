22f41298fa401abdec0869c9297cca4082ed7944
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-04-08 16:17:57 +0200

    add first exercice

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 10:35:54 by jodone            #+#    #+#             */
+/*   Updated: 2026/04/08 16:15:42 by jodone           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctype.h>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for(int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
